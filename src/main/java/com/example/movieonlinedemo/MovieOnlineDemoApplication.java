6d0a545566b57388635196545d177579e6ce5528
TheShuai <1019818712@qq.com>
2023-03-01 21:11:37 +0800

    Initial commit

---
+++ src/main/java/com/example/movieonlinedemo/MovieOnlineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieonlinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieOnlineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieOnlineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieonlinedemo/MovieOnlineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieonlinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieOnlineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
